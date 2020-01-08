925d590d625648c1e7798eb0a46b65c4dd2c1876
Matheus Gomes <matheusgomes062@gmail.com>
2020-01-08 17:19:46 -0300

    creating class ChessPosition

---
+++ console-chess/board/chess/ChessPosition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace console_chess.board.chess
+{
+    class ChessPosition
+    {
+        public char column { get; set; }
+        public int row { get; set; }
+
+        public ChessPosition(char column, int row)
+        {
+            this.column = column;
+            this.row = row;
+        }
+
+        public Position toPosition()
+        {
+            return new Position(8 - row, column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + column + row;
+        }
+    }
+}
